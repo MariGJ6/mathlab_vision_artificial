fbe64b2cc1a9e76872f05716a7574ba394af96aa
MariGJ6 <132412503+MariGJ6@users.noreply.github.com>
2023-05-04 19:30:00 -0600

    Add files via upload

---
+++ Camara_bordes.m
@@ -0,0 +1,17 @@
+clear all
+close all
+ clc
+
+ webcamlist();
+
+ cam = webcam(1);
+ 
+ while true
+
+     img = snapshot(cam);
+     img = rgb2gray(img);
+     img = edge(img,"canny");
+
+     figure(1)
+     imshow(img)
+ end
